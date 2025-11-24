0facb9866b1fe863fe9cba20d17c8b78b045d778
pcodefromOarch <primecoder8@gmail.com>
2025-11-24 23:49:40 +0530

    add auth route for user

--- handler/handler.go
+++ handler/handler.go
@@ -1,8 +1,10 @@
 package handler
 
 import (
+	"fmt"
 	"mmo-backend/helper"
 	atypes "mmo-backend/types"
+	"strings"
 	"time"
 
 	"github.com/go-playground/validator/v10"
@@ -29,9 +31,12 @@ func LoginHandler(c *fiber.Ctx) error {
 			"detail":  "error validating request body",
 		})
 	}
-	claims := &jwt.RegisteredClaims{
-		ExpiresAt: jwt.NewNumericDate(time.Unix(1516239022, 0)),
-		Issuer:    req.Username,
+	claims := &MyClaims{
+		Username: req.Username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	secret := helper.GetConfig()
@@ -46,6 +51,56 @@ func LoginHandler(c *fiber.Ctx) error {
 	return c.Status(200).JSON(fiber.Map{
 		"message": "success",
 		"token":   ss,
+	})
+
+}
+
+type MyClaims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func LoginCheck(c *fiber.Ctx) error {
+	secret := helper.GetConfig()
+	authHeader := c.Get("Authorization")
+	if authHeader == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Missing Authorization header",
+		})
+	}
+
+	parts := strings.Split(authHeader, " ")
+	if len(parts) != 2 || parts[0] != "Bearer" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Invalid Authorization header format",
+		})
+	}
+
+	t := parts[1]
+	token, err := jwt.ParseWithClaims(t, &MyClaims{}, func(token *jwt.Token) (interface{}, error) {
+		// Validate signing algorithm
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secret.JwtSecret), nil
+	})
+
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "token parsing error",
+			"error":   err.Error(),
+		})
+	}
+
+	claims, ok := token.Claims.(*MyClaims)
+	if !ok || !token.Valid {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "invalid token",
+		})
+	}
+	return c.Status(200).JSON(fiber.Map{
+		"message": "success",
+		"claims":  claims.Username,
 	})
 
 }
--- router/router.go
+++ router/router.go
@@ -2,14 +2,16 @@ package router
 
 import (
 	"fmt"
-	"github.com/gofiber/fiber/v2"
 	"log"
 	"mmo-backend/handler"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func StartRouter() {
 	app := fiber.New()
 	app.Post("/login", handler.LoginHandler)
+	app.Get("/me", handler.LoginCheck)
 	fmt.Println("Listening on port 3000")
 	log.Fatal(app.Listen(":3000"))
 }
