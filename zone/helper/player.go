f2ea1441f9bf6b5b612f10854cc3002f70701f57
saadfromwin11l <primecoder8@gmail.com>
2025-12-02 13:58:12 +0000

    add some value in struct

--- zone/helper/player.go
+++ zone/helper/player.go
@@ -1,11 +1,19 @@
 package zoneHelper
 
 import "github.com/gorilla/websocket"
+// 1 unit means one meter
+// 2 server tick interval is 100ms 
 
 type Player struct {
 	Name string
+	// position x , y 
 	X 	int
 	Y 	int
+	// last time when clients input was processed
+	LastUpdate int64
+	// last time when update was sent to the client 
+	LastSent  int64``
+	isMoving 	bool
 	Conn  *websocket.Conn `json:"-"`
 }
 
